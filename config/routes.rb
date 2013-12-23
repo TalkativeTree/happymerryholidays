75bc15f99e3b78a1062be587f2f1ef53be809b6f
Ben Angel <benjamin.b2@gmail.com>
2013-12-22 23:59:40 -0800

    removed namespaces and used :type instead for routes. big improvement on length of routes and much simpler

--- config/routes.rb
+++ config/routes.rb
@@ -5,27 +5,7 @@ Turkeythanks::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
-  namespace :thanksgiving do
-    resources :cards, only: [:new, :create, :show]
-  end
-
-  namespace :christmas do
-    resources :cards, only: [:new, :create, :show]
-  end
-
-  namespace :hanukkah do
-    resources :cards, only: [:new, :create, :show]
-  end
-
-  # post 'thanksgiving/new' => 'catalog#new'
-  # post 'christmas/new' => 'catalog#new'
-  # post 'hanukkah/new' => 'catalog#new'
-
-  # get 'thanksgiving' => 'catalog#thanksgreeting'
-  # get 'christmas' => 'catalog#christmas'
-  # get 'hanukkah' => 'catalog#hanukkah'
-
-  # get 'thanksgiving/:url' => 'catalog#show'
-  # get 'christmas/:url' => 'catalog#show'
-  # get 'hanukkah/:url' => 'catalog#show'
+  get  ':type/new'    => 'cards#new'
+  post ':type/create' => 'cards#create'
+  get  ':type/:url'   => 'cards#show'
 end
