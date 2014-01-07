ba56513dc0878d675846ee06c382d57fd73ce778
Ben Angel <benjamin.b2@gmail.com>
2014-01-06 22:37:51 -0800

    added validations to Card for url and message

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,21 @@
 class Card < ActiveRecord::Base
+  @reserved_words = ["new","create","show"]
+
+  validate :url, presence: true
+  validate :url, uniqueness: true
+  validates :url, :exclusion => { :in => @reserved_words, :message => "\"%{value}\" is reserved."}
+
+  validate :message, presence: true
+  after_validation :default_values
+
+  protected
+  def default_values
+    if no_url?
+      self.url = SecureRandom.hex(5)
+    end
+  end
+
+  def no_url?
+    self.url.length < 1 || self.url == ''
+  end
 end
